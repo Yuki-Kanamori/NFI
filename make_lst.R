b8cefb3f0e10f94db6666a80feaff4ac96077090
Yuki-Kanamori <y.kanamory@gmail.com>
2024-04-12 20:27:38 +0900

    revised

--- make_lst.R
+++ make_lst.R
@@ -12,7 +12,7 @@ require(tidyverse)
 # file = paste0("/Users/Yuki/Dropbox/LST/mean/AMDy1999p3623eTMP_max.nc.nc4")
 # GET(url, authenticate("kanayuki", "CU8D0qye2024"), write_disk(file))
 
-loc_lst = read.csv("loc_lst.csv", fileEncoding = "CP932") %>% rename(tag = tag2)
+loc_lst = read.csv("/Users/Yuki/Dropbox/LST/mean/loc_sp.csv", fileEncoding = "CP932") 
 since = ymd("1900-01-01")
 
 a1 = NULL
@@ -25,25 +25,17 @@ for(i in 1999:2024){
   T <- ncvar_get(nc, "TMP_mea")
   # dim(T) #[1] 560 800 365 lon, lat, time
   
-  # time = seq(as.Date(paste0(year, "-01-01")), as.Date(paste0(year, "-12-31")), by="days")
-  # length = length(time)
-  
-  # 閏年はなぜかデータがないから2/29を除去する
-  # if(length == 366){
-  #   time = time[-60]
-  # }
-  
   time = data.frame(number = nc[["dim"]][["time"]][["vals"]]) %>% mutate(time = since + days(number))
   
   temp = NULL
   year_all = NULL
-  for(j in 1:length(time)){
+  for(j in 1:nrow(time)){
     temp = data.frame(T[, , j])
     colnames(temp) = nc[["dim"]][["lat"]][["vals"]]
     temp$lon = nc[["dim"]][["lon"]][["vals"]]
     
     temp2 = temp %>% gather(key = lat, value = lst, -lon) %>% mutate(time = paste(time[j, "time"])) %>% na.omit() %>%
-      mutate(year = as.numeric(str_sub(time, 1, 4)), month = as.numeric(str_sub(time, 6, 7)), day = as.numeric(str_sub(time, 9, 10)), tag = paste(lon, lat, sep = "_"))
+      mutate(year = as.numeric(str_sub(time, 1, 4)), month = as.numeric(str_sub(time, 6, 7)), day = as.numeric(str_sub(time, 9, 10)), tag2 = paste(lon, lat, sep = "_"))
     
     loc = loc_lst
     temp3 = left_join(loc, temp2, by = "tag2")
@@ -51,19 +43,10 @@ for(i in 1999:2024){
     year_all = rbind(year_all, temp3)
   }
   
-  # if(year == 1999){
-  #   year_all = year_all %>% filter(month > 3)
-  # }
-  # if(year == 2024){
-  #   year_all = year_all %>% filter(month < 4)
-  # }
-  
-  tag = year_all %>% select(lon, lat, tag) %>% distinct(tag, .keep_all = TRUE)
   year_all2 = year_all %>% mutate(nendo = ifelse(month < 4, (as.numeric(i)-1), i))
-  year_all3 = year_all2 %>% group_by(tag, nendo) %>% summarize(mean = mean(lst))
-  year_all4 = left_join(tag, year_all3, by = "tag") %>% select(-tag)
+  year_all3 = year_all2 %>% group_by(no, nendo) %>% summarize(mean = mean(lst))
   
-  a1 = rbind(a1, year_all4)
+  a1 = rbind(a1, year_all3)
 }
 
 
