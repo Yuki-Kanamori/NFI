b5cbd969da3cf839351783ed1e8d5b3510b87fc9
Yuki-Kanamori <y.kanamory@gmail.com>
2024-10-04 11:14:28 +0900

    initial commit

---
+++ dist_shifts/shifts.R
@@ -0,0 +1,104 @@
+require(tidyverse)
+
+
+# directory ---------------------------------------------------------------
+dir = "/Users/Yuki/Dropbox/NFI"
+
+
+# data --------------------------------------------------------------------
+setwd(dir = dir)
+load("df_allN.Rdata") # df_all
+
+
+# 都道府県情報 ------------------------------------------------------------------
+# 1期
+dir_data1 = "/Users/Yuki/Dropbox/NFI/NFI_1_CSV/第1期"
+setwd(dir_data1)
+
+pref1 = read.csv("01_プロット.csv", fileEncoding = "CP932") %>% select("格子点ID", "都道府県", "土地利用")
+colnames(pref1) = c("site_id", "pref", "usage")
+
+elev1 = read.csv("02_様式２−１.csv", fileEncoding = "CP932") %>% select("格子点ID","標高") %>% rename(site_id = 格子点ID, elevation = 標高)
+
+pref1 = left_join(pref1, elev1, by = "site_id")
+pref1$time = 1
+
+
+# 2期
+dir_data2 = "/Users/Yuki/Dropbox/NFI/NFI_2_CSV/第2期"
+setwd(dir_data2)
+
+pref2 = read.csv("01_プロット.csv", fileEncoding = "CP932") %>% select("格子点ID", "都道府県", "土地利用")
+colnames(pref2) = c("site_id", "pref", "usage")
+
+elev2 = read.csv("02_様式２−１.csv", fileEncoding = "CP932") %>% select("格子点ＩＤ","標高") %>% rename(site_id = 格子点ＩＤ, elevation = 標高)
+
+pref2 = left_join(pref2, elev2, by = "site_id")
+pref2$time = 2
+
+
+# 3期
+dir_data3 = "/Users/Yuki/Dropbox/NFI/NFI_3_CSV/第3期"
+setwd(dir_data3)
+
+pref3 = read.csv("01_プロット.csv", fileEncoding = "CP932") %>% select("格子点ID", "都道府県", "土地利用")
+colnames(pref3) = c("site_id", "pref", "usage")
+
+elev3 = read.csv("19_様式７.csv", fileEncoding = "CP932") %>% select("格子点ＩＤ","標高") %>% rename(site_id = 格子点ＩＤ, elevation = 標高)
+
+pref3 = left_join(pref3, elev3, by = "site_id")
+pref3$time = 3
+
+
+# 4期
+dir_data4 = "/Users/Yuki/Dropbox/NFI/NFI_4_CSV/第4期"
+setwd(dir_data4)
+
+pref4 = read.csv("01_プロット.csv", fileEncoding = "CP932") %>% select("格子点ID", "都道府県", "土地利用")
+colnames(pref4) = c("site_id", "pref", "usage")
+
+elev4 = read.csv("21_様式７.csv", fileEncoding = "CP932") %>% select("格子点ＩＤ","標高") %>% rename(site_id = 格子点ＩＤ, elevation = 標高)
+
+pref4 = left_join(pref4, elev4, by = "site_id")
+pref4$time = 4
+
+
+df_pref = rbind(pref1, pref2, pref3, pref4) %>% distinct(site_id, time, .keep_all = TRUE)
+nrow(pref1)+nrow(pref2)+nrow(pref3)+nrow(pref4)
+
+
+
+# 樹木データに都道府県と土地利用情報を付与する --------------------------------------------------
+df_all2 = left_join(df_all, df_pref, by = c("site_id", "time")) %>% filter(usage == "森林")
+unique(df_all2$pref)
+
+
+# 重心の変化 -------------------------------------------------------
+tohoku = c("青森", "岩手", "秋田", "宮城", "福島", "山形")
+tohoku = c("長崎県", "鹿児島県", "佐賀県", "熊本県", "宮崎県", "大分県", "福岡県")
+
+df_tohoku = df_all2 %>% filter(pref %in% tohoku)
+unique(df_tohoku$pref)
+
+pre_cog = df_tohoku %>% group_by(time, site_id, lon, lat, species) %>% summarize(count = sum(n)) %>% mutate(effort_ha = 0.1) %>% mutate(dens = count/effort_ha)
+
+# 緯度方向
+haha_lat = pre_cog %>% group_by(time) %>% summarize(haha = sum(dens))
+ko_lat = pre_cog %>% mutate(ko = dens*lat) %>% group_by(time, species) %>% summarize(ko = sum(ko))
+cog_lat = left_join(ko_lat, haha_lat, by = "time") %>% mutate(cog = ko/haha)
+
+length(unique(ko_lat$species))
+
+sp = unique(ko_lat$species)
+df_lm = NULL
+for(i in 1:1313){
+  df = cog_lat %>% ungroup() %>% filter(species == sp[[i]])
+  if(sum(df$cog) == 0) next
+  lm = summary(lm(cog ~ time, data = df))
+  lm2 = lm[["coefficients"]] %>% data.frame() %>% mutate(species = sp[[i]], param = c("intercept", "time"))
+  
+  df_lm = rbind(df_lm, lm2)
+}
+
+inc_lat = df_lm %>% filter(param == "time", Pr...t.. <= 0.05, Estimate > 0)
+
