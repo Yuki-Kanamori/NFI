fa17241b1a22872bf6ceb76efb4af4637eb5514f
Yuki Kanamori <yuki@YukinoMacBook-Pro.local>
2025-03-27 08:53:54 +0900

    revised

--- getagr.R
+++ getagr.R
@@ -15,13 +15,50 @@ library(agrmesh)
 
 
 # data --------------------------------------------------------------------
+# 取得可能なデータの種類
+preview_dataset("daily")
+
+# 取得可能な地理情報データ
+# https://amu.rd.naro.go.jp/wiki_open/doku.php?id=geodata
+print(preview_dataset("geo"), n = 100)
+# point_altitude <-
+#   fetch_amgsds(
+#     lats = sites$lat, lons = sites$lon,    
+#     elements = "altitude",                  
+#     source = "geo"
+#   )
+
+
+
+# データのダウンロード --------------------------------------------------------------
+for(i in 1980:2024){
+  start = paste0(i, "-01-01")
+  end = paste0(i, "-12-31")
+  paths <-
+    generate_path(
+      times = ymd(c(start, end), tz = "Japan"),
+      lats = c(24, 46), 
+      lons = c(122, 146),
+      element = "TMP_mea",
+      source = "daily",
+      is_clim = FALSE
+    )
+  setwd(dir = "/Users/Yuki/Dropbox/LST")
+  dir.create(paste(i))
+  outdir = paste0("/Users/Yuki/Dropbox/LST/", i)
+  download_netcdf(amgsds_path = paths$complete, server = "amd.rd.naro.go.jp/opendap", outdir = outdir, .silent = FALSE)
+}
+
+
+
+
 area_daily_lst =
   fetch_amgsds(
-    times = ymd(c("1980-01-01", "1989-12-31"), tz = "Japan"),
+    times = ymd(c("1980-01-01", "1980-01-02"), tz = "Japan"),
     lats = c(24, 46), lons = c(122, 146),
     elements = "TMP_mea",
     mode = "area",
-    output = "tibble"
+    output = "array"
   )
 
 # area_daily_rain <-
