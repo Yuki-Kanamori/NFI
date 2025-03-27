f8f9ebe18021cf0389373f9568a5d32814abceaf
Yuki Kanamori <yuki@YukinoMacBook-Pro.local>
2025-03-27 09:42:19 +0900

    revised

--- getagr.R
+++ getagr.R
@@ -43,29 +43,39 @@ for(i in 1980:2024){
       source = "daily",
       is_clim = FALSE
     )
-  setwd(dir = "/Users/Yuki/Dropbox/LST")
-  dir.create(paste(i))
-  outdir = paste0("/Users/Yuki/Dropbox/LST/", i)
+  # setwd(dir = "/Users/Yuki/Dropbox/LST")
+  # dir.create(paste(i))
+  outdir = paste0("/Users/Yuki/Dropbox/LST/")
   download_netcdf(amgsds_path = paths$complete, server = "amd.rd.naro.go.jp/opendap", outdir = outdir, .silent = FALSE)
+}
+
+area_daily_lst =
+  fetch_amgsds(
+    times = ymd(c("1980-01-01", "1980-01-01"), tz = "Japan"),
+    lats = c(24, 46), lons = c(122, 146),
+    elements = "TMP_mea",
+    mode = "area",
+    output = "tibble"
+  )
+
+lonlat = area_daily_lst %>% mutate(site_id = 1:nrow(area_daily_lst))
+
+for(i in 1:nrow(lonlat)){
+  lon = lonlat[i, "lon"]
+  lat = lonlat[i, "lat"]
+
+  temp = fetch_amgsds(
+    times = ymd(c("1980-01-01", "2024-12-31"), tz = "Japan"),
+    lats = lat$lat, lons = lon$lon,
+    elements = "TMP_mea",
+    output = "tibble"
+  ) %>% mutate(site_id = i)
+  
+  setwd("/Users/Yuki/Dropbox/LST/1980-2024")
+  file_name = paste0("TMP_mea_site", i, ".csv")
+  write.csv(temp, file_name, row.names = FALSE)
 }
 
 
 
-
-area_daily_lst =
-  fetch_amgsds(
-    times = ymd(c("1980-01-01", "1980-01-02"), tz = "Japan"),
-    lats = c(24, 46), lons = c(122, 146),
-    elements = "TMP_mea",
-    mode = "area",
-    output = "array"
-  )
-
-# area_daily_rain <-
-#   fetch_amgsds(
-#     times = ymd(c("2023-01-10", "2023-01-11"), tz = "Japan"),
-#     lats = c(41.5, 43.8), lons = c(140.2, 142.0),
-#     elements = "APCP",
-#     mode = "area",
-#     output = "array"
-#   )
+setwd("/Users/Yuki/Dropbox/LST/mean")
